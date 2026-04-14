2d8c0952f750a073a1465707eaae13dc55a7d3b7
rohitroy17-gif <roy172758@gmail.com>
2026-04-14 12:13:05 +0600

    change done

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 import streamlit as st
 import google.generativeai as genai
+import google.api_core.exceptions
 
 st.title("🤖 Gemini Q&A App")
 
+# API key from Streamlit secrets
 api_key = st.secrets["GEMINI_API_KEY"]
-
 genai.configure(api_key=api_key)
 
+# Model
+model = genai.GenerativeModel("gemini-3-flash-preview")
+
+# Input
 question = st.text_input("Ask your question:")
 
+# Button
 if st.button("Ask Gemini"):
+
     if question.strip():
+
+        prompt = question
+
         try:
-            model = genai.GenerativeModel("gemini-3-flash-preview")
-            response = model.generate_content(question)
+            response = model.generate_content(prompt)
 
             st.markdown("### 📢 Answer:")
             st.markdown(response.text)
 
+        except google.api_core.exceptions.ResourceExhausted:
+            st.error("⚠️ Too many requests! You hit the Free Tier limit. Try again later.")
+
         except Exception as e:
-            st.error(f"Error: {e}")
+            st.error(f"❌ Unexpected error: {e}")
+
     else:
         st.warning("Please enter a question")
